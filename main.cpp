8f400d57a59a6d597830e1084477d2a90ee96fc2
Antonio Ganea <antonio.ganea@yahoo.com>
2020-03-29 22:57:22 +0300

    Merge sort

--- main.cpp
+++ main.cpp
@@ -36,6 +36,67 @@ void countsort(int * v, int length, int maxNo ){
     }
 }
 
+
+void merge(int * v, int left, int mid, int right) 
+{ 
+    int i, j, k; 
+    int n1 = mid - left + 1; 
+    int n2 =  right - mid; 
+  
+    int L[n1], R[n2]; 
+  
+    for (i = 0; i < n1; i++) 
+        L[i] = v[left + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = v[mid + 1 + j]; 
+  
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            v[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            v[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+
+    while (i < n1) 
+    { 
+        v[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+
+    while (j < n2) 
+    { 
+        v[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+}
+
+void mergesort(int * v, int left, int right) 
+{ 
+    if (left < right) 
+    { 
+        int mid = (left+right)/2; 
+
+        mergesort(v, left, mid); 
+        mergesort(v, mid+1, right); 
+  
+        merge(v, left, mid, right); 
+    } 
+} 
+
+
 FILE * fin;
 
 void printVector(int * v, int length){
@@ -81,6 +142,15 @@ int main (){
         chrono::steady_clock::time_point end = chrono::steady_clock::now();
         printVector(tempVector,N);
         cout << "Count sort - duration " << chrono::duration_cast<chrono::nanoseconds>(end-start).count() << " nanoseconds" << endl;
+        }
+
+        {
+        memcpy(tempVector, V, N * sizeof(int));
+        chrono::steady_clock::time_point start = chrono::steady_clock::now();
+        mergesort(tempVector,0,N-1);
+        chrono::steady_clock::time_point end = chrono::steady_clock::now();
+        printVector(tempVector,N);
+        cout << "Merge sort - duration " << chrono::duration_cast<chrono::nanoseconds>(end-start).count() << " nanoseconds" << endl;
         }
 
         delete[] tempVector;
