4d7c1cc721e6033e628e38b8daca2a8098d84947
Antonio Ganea <antonio.ganea@yahoo.com>
2020-03-29 22:10:29 +0300

    Bubble sort initial commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 using namespace std;
 
+int V[10000];
+
+void bubblesort(int * v, int length){
+    bool unsorted = true;
+    while ( unsorted ){
+        unsorted = false;
+        for ( int i = 0; i < length-1; i++ ){
+            if ( v[i] < v[i+1] ){
+                int temp = v[i];
+                v[i] = v[i+1];
+                v[i+1] = v[i];
+                unsorted = true;
+            }
+        }
+    }
+}
+
+
+
 
 int main (){
-
-
 
     return 0;
 }
